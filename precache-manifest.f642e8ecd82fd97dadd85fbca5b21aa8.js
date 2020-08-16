7cf26f05284788f5cc670d59b72f4d0c8d9a1daa
elencrisf <elen.crisf@uol.com.br>
2020-08-15 22:28:10 -0700

    Updates

---
+++ precache-manifest.f642e8ecd82fd97dadd85fbca5b21aa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6399774c92d4cd03cf67492ec943f5",
+    "url": "/project-overview-react/index.html"
+  },
+  {
+    "revision": "73dffec8e24104ff1255",
+    "url": "/project-overview-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "255d07115f15040f8dc3",
+    "url": "/project-overview-react/static/css/main.fd73f512.chunk.css"
+  },
+  {
+    "revision": "73dffec8e24104ff1255",
+    "url": "/project-overview-react/static/js/2.bf17251a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/project-overview-react/static/js/2.bf17251a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255d07115f15040f8dc3",
+    "url": "/project-overview-react/static/js/main.0e4f4bdf.chunk.js"
+  },
+  {
+    "revision": "41515da428a44c53e09b",
+    "url": "/project-overview-react/static/js/runtime-main.3b760315.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/project-overview-react/static/media/boatImage.d61546cd.jpg"
+  }
+]);
